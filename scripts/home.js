fde024aa1984b99a62da82de7b3ea8886e0ab96f
helloredreyes <hello.silentlymine@gmail.com>
2024-07-06 11:35:03 +0700

    sync

--- scripts/home.js
+++
@@ -1,8 +0,0 @@
-// mute video
-document.addEventListener('DOMContentLoaded', function() {
-    var video = document.querySelector('#background-video video');
-    video.muted = true;
-    video.autoplay = true;
-    video.classList.add('show');
-  });
-  
