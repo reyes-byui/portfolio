74dcfefd4886fcedd1a6e5d78bf3d91009948334
helloredreyes <hello.silentlymine@gmail.com>
2024-07-26 01:01:28 +0700

    sync

--- scripts/insights.js
+++ scripts/insights.js
@@ -1,29 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var menuToggle = document.querySelector('.menu-toggle');
-  var menuLinks = document.querySelector('.menu-links');
-  menuToggle.addEventListener('click', function() {
-      menuLinks.classList.toggle('show');
-  });
-  
-  const titleElements = document.querySelectorAll('.title-animation, .h4-animation');
-  const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-          if (entry.isIntersecting) {
-              entry.target.classList.add('visible');
-              observer.unobserve(entry.target);
-          }
-      });
-  }, {
-      threshold: 0.1 
-  });
-
-  titleElements.forEach(element => {
-      observer.observe(element);
-  });
-});
-
-
-// local storage
 document.addEventListener("DOMContentLoaded", function() {
   const form = document.getElementById("contactForm");
 
